d3c431a80e3424fd8221ebc4d8548be157fc281a
Alvin Vogelzang <alvinvogelzang@gmail.com>
2014-05-04 21:42:33 +0200

    Changed ip to localhost

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 var app = angular.module("chatApp", []);
 
 app.factory('socket', function(){
-	var socket = io.connect('http://192.168.2.6:3000');
+	var socket = io.connect('http://localhost:3000');
 	return socket;
 })
 

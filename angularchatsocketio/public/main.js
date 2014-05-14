5110b32fa8bab9291cb8975d4f7dd97ffe0a466b
Alvin Vogelzang <alvinvogelzang@gmail.com>
2014-05-15 00:15:09 +0200

    Angularchat socket io in 'angularchatsocketio' folder

---
+++ angularchatsocketio/public/main.js
@@ -0,0 +1,23 @@
+var app = angular.module("chatApp", []);
+
+app.factory('socket', function(){
+	var socket = io.connect('http://localhost:3000');
+	return socket;
+})
+
+app.controller('ChatCtrl', function($scope, socket){
+	$scope.msgs = [];
+	$scope.sendMsg = function(){
+		socket.emit('send msg', {
+			name: $scope.msg.name + ' zegt:',
+			text:  $scope.msg.text,
+		});
+		$scope.msg.text = "";
+	}
+
+	socket.on('get msg', function(data){
+		$scope.msgs.push(data);
+		$scope.$digest();
+		console.log($scope.msgs);
+	});
+});
